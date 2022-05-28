32dd6843815a899bb038c437d99fb9579db9f91f
karlapa06 <105948300+karlapa06@users.noreply.github.com>
2022-05-27 21:38:57 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ Karla Alejandra Padilla González
+ A01705331
+ Proyecto de la clase TC1030
+*/
+
+#include <iostream>
+#include "restaurante.h"
+#include "cliente.h"
+#include "pedido.h"
+
+using namespace std;
+ 
+int main ( ) {
+    
+}
